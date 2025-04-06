839df2eaaa59457a1f105d7198668d808a15a4fb
Simón Paipa <simonpaipab@gmail.com>
2025-04-06 16:29:47 -0500

    Primer login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage {
+  email = '';
+  password = '';
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  login() {
+    if (this.email === 'admin@example.com' && this.password === '1234') {
+      alert('Inicio de sesión exitoso');
+      // Aquí podrías redirigir a otra página si decides crearla luego
+    } else {
+      alert('Credenciales incorrectas');
+    }
+  }
 }
