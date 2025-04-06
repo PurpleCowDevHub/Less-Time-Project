bc2df20802bb4c44ee4631811f2a4960ef9b5afe
Simón Paipa <simonpaipab@gmail.com>
2025-04-06 17:46:06 -0500

    Retroceso

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,28 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss']
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
 })
 export class HomePage {
-  email = '';
-  password = '';
 
-  constructor(private router: Router) {}
+  constructor() {}
 
-  login() {
-    if (this.email === 'admin@example.com' && this.password === '1234') {
-      alert('Inicio de sesión exitoso');
-      // Aquí podrías redirigir a otra página si decides crearla luego
-    } else {
-      alert('Credenciales incorrectas');
-    }
-  }
 }
