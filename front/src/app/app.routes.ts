c6a6ae2d8c6c99633e0afcd785c750442ad52569
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2025-05-19 17:02:49 -0500

    Loginscss_corregido

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -17,5 +17,9 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },  {
+    path: 'principal',
+    loadComponent: () => import('./pages/principal/principal.page').then( m => m.PrincipalPage)
   },
+
 ];
