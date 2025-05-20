37c4d780807feff75709d1b0b5a2268ace75bdfe
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2025-05-20 13:08:42 -0500

    pagina home

--- front/src/app/pages/home/home.page.spec.ts
+++ front/src/app/pages/home/home.page.spec.ts
@@ -1,12 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { PrincipalPage } from './principal.page';
+import { HomePage } from './home.page';
 
-describe('PrincipalPage', () => {
-  let component: PrincipalPage;
-  let fixture: ComponentFixture<PrincipalPage>;
+describe('HomePage', () => {
+  let component: HomePage;
+  let fixture: ComponentFixture<HomePage>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PrincipalPage);
+    fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
