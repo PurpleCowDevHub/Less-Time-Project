be112fb8721fe04e71a60b818940f1075ca36db8
Simón Paipa <simonpaipab@gmail.com>
2025-05-20 21:44:58 -0500

    sin dirección principal 2

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -18,8 +18,4 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
-  {
-    path: 'principal',
-    loadComponent: () => import('./pages/principal/principal.page').then( m => m.PrincipalPage)
-  },
 ];
