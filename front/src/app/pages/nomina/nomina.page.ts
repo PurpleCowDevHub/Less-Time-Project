15faca43cbcf6ea24357f48378cde63a25738b33
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2025-05-24 17:35:17 -0500

    hipervinculos y casilla nomina

--- front/src/app/pages/nomina/nomina.page.ts
+++ front/src/app/pages/nomina/nomina.page.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ModalController, IonicModule } from '@ionic/angular';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-nomina',
   templateUrl: './nomina.page.html',
   styleUrls: ['./nomina.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class NominaPage {
   grupos = [
@@ -30,8 +28,57 @@ export class NominaPage {
     },
   ];
 
-  abrirPerfil() {
-    console.log('Abriendo perfil...');
+  idNomina: string = '';
+  usuarioId: string = '';
+  horasTrabajadas: number = 0;
+  diasIncapacidad: number = 0;
+  horasExtra: number = 0;
+  bonificacion: number = 0;
+  periodoPago: string = '';
+  idEnvioNomina: string = '';
+
+  irAPrincipal() {
+    window.location.href = 'http://localhost:8101/principal';
+  }
+
+  irAEmpleados() {
+    window.location.href = 'http://localhost:8100/listausuarios';
+  }
+
+  irACalendario() {
+    window.location.href = 'http://localhost:8101/horario';
+  }
+
+  irANomina() {
+    window.location.href = 'http://localhost:8101/nomina';
+  }
+
+  irAPerfilAdmin() {
+    window.location.href = 'http://localhost:8100/perfiladmin';
+  }
+
+  guardarNomina() {
+    console.log(`Guardando nómina con ID: ${this.idNomina}`);
+  }
+
+  guardarDatosNomina() {
+    console.log('Datos para nómina:', {
+      usuarioId: this.usuarioId,
+      horasTrabajadas: this.horasTrabajadas,
+      diasIncapacidad: this.diasIncapacidad,
+      horasExtra: this.horasExtra,
+      bonificacion: this.bonificacion,
+      periodoPago: this.periodoPago,
+    });
+  }
+
+  enviarNominaConDatos() {
+    console.log('Enviando nómina con ID:', this.idEnvioNomina);
+  }
+
+  enviarNomina() {
+    console.log('Enviar nómina básico con ID:', this.idNomina);
   }
 }
+
 
