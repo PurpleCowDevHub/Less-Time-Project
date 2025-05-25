bfbfa6891196ccc90c94bbbe43910f6cc2a713c5
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2025-05-25 13:54:20 -0500

    correcion nueva jornada

--- front/src/app/pages/nuevajornada/nuevajornada.page.ts
+++ front/src/app/pages/nuevajornada/nuevajornada.page.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalController, IonicModule } from '@ionic/angular';
-import { ReactiveFormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-nuevajornada',
@@ -13,11 +11,45 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class NuevajornadaPage implements OnInit {
+  dia: string = '';
+  mes: string = '';
+  anio: string = '';
+  maxAnio: number = new Date().getFullYear();
+
+  // Nuevas propiedades para horas
+  horaEntrada: string = '';
+  horaSalida: string = '';
+
   constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {}
 
   cerrarModal() {
     this.modalCtrl.dismiss();
+  }
+
+  validarDia() {
+    if (this.dia) {
+      const dayNum = parseInt(this.dia, 10);
+      if (dayNum < 1) this.dia = '1';
+      if (dayNum > 31) this.dia = '31';
+    }
+  }
+
+  validarAnio() {
+    if (this.anio) {
+      const yearNum = parseInt(this.anio, 10);
+      if (yearNum < 1950) this.anio = '1950';
+      if (yearNum > this.maxAnio) this.anio = this.maxAnio.toString();
+    }
+  }
+
+  obtenerFecha(): Date | null {
+    if (this.dia && this.mes && this.anio) {
+      const fechaStr = `${this.anio}-${this.mes}-${this.dia}`;
+      const fecha = new Date(fechaStr);
+      return isNaN(fecha.getTime()) ? null : fecha;
+    }
+    return null;
   }
 }
