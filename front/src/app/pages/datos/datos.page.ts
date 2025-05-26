667177c3fd1afd1fb86cd790482b9f6ea6ca3de9
Isabella Diaz <est.isabella.diazp@unimilitar.edu.co>
2025-05-25 22:06:13 -0500

    mejorar: actualizar el servicio de usuario para obtener métricas y ajustar la importación de Chart.js en la página de datos

--- front/src/app/pages/datos/datos.page.ts
+++ front/src/app/pages/datos/datos.page.ts
@@ -1,7 +1,28 @@
-  import { Component, OnInit } from '@angular/core';
-  import { CommonModule } from '@angular/common';
-  import { FormsModule } from '@angular/forms';
-  import {
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonButton,
+  IonAvatar
+} from '@ionic/angular/standalone';
+import { UserService } from 'src/app/services/user.service';
+import { Chart } from 'chart.js/auto';
+
+@Component({
+  selector: 'app-datos',
+  templateUrl: './datos.page.html',
+  styleUrls: ['./datos.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
     IonContent,
     IonHeader,
     IonTitle,
@@ -11,85 +32,77 @@
     IonIcon,
     IonButton,
     IonAvatar
-  } from '@ionic/angular/standalone';
+  ]
+})
+export class DatosPage implements OnInit {
+  data: any;
+  edadKeys: string[] = [];
+  ventasKeys: string[] = [];
 
-  @Component({
-    selector: 'app-datos',
-    templateUrl: './datos.page.html',
-    styleUrls: ['./datos.page.scss'],
-    standalone: true,
-    imports: [
-      CommonModule,
-      FormsModule,
-      IonContent,
-      IonList,
-      IonItem,
-      IonIcon,
-      IonButton,
-      IonAvatar
-    ]
-  })
-  export class DatosPage implements OnInit {
-    data: any;
-    edadKeys: string[] = [];
-    ventasKeys: string[] = [];
+  constructor(private userService: UserService) {}
 
-    constructor() {}
-
-    ngOnInit() {
-      // Datos de nómina simulados
-      this.data = {
-        salario_bruto_promedio: "$4,406,666.67",
-        salario_bruto_sumatoria: "$13,220,000.00",
-        salario_neto_promedio: "$4,054,133.33",
-        salario_neto_sumatoria: "$12,162,400.00",
-        horas_trabajadas_promedio: 63.33,
-        horas_trabajadas_sumatoria: 190,
-        bonificacion_promedio: "$53,333.33",
-        bonificacion_sumatoria: "$160,000.00",
-        edad_promedio: 21.5,
-        edad_sumatoria: 129,
-        distribucion_edades: {
-          "<20": 0,
-          "20-29": 6,
-          "30-39": 0,
-          "40-49": 0,
-          "50-59": 0,
-          "60+": 0
-        },
-        ventas_simuladas_por_mes: {
-          "2025-01": 31304.24,
-          "2025-02": 41512.04,
-          "2025-03": 39687.84,
-          "2025-04": 43137.17,
-          "2025-05": 42745.38,
-          "2026-12": 30799.54
-        }
-      };
-
-      // Claves necesarias para iterar en *ngFor
+  ngOnInit() {
+    this.userService.obtenerMetricas().subscribe((res) => {
+      this.data = res;
       this.edadKeys = Object.keys(this.data.distribucion_edades);
       this.ventasKeys = Object.keys(this.data.ventas_simuladas_por_mes);
-    }
 
-    // Funciones de navegación
-    irAPrincipal() {
-      window.location.href = 'http://localhost:8100/home';
-    }
+      this.renderizarGraficaEdad();
+    });
+  }
 
-    irAEmpleados() {
-      window.location.href = 'http://localhost:8100/empleados';
-    }
-
-    irACalendario() {
-      window.location.href = 'http://localhost:8100/calendario';
-    }
-
-    irANomina() {
-      window.location.href = 'http://localhost:8100/nomina';
-    }
-
-    irAPerfilAdmin() {
-      window.location.href = 'http://localhost:8100/perfiladmin';
+  renderizarGraficaEdad() {
+    const canvas = document.getElementById('edadChart') as HTMLCanvasElement;
+    if (canvas) {
+      new Chart(canvas, {
+        type: 'bar',
+        data: {
+          labels: this.edadKeys,
+          datasets: [{
+            label: 'Cantidad',
+            data: this.edadKeys.map(k => this.data.distribucion_edades[k]),
+            backgroundColor: 'rgba(66, 133, 244, 0.5)',
+            borderColor: 'rgba(66, 133, 244, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false
+            },
+            title: {
+              display: false
+            }
+          },
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
     }
   }
+
+  irAPrincipal() {
+    window.location.href = 'http://localhost:8100/home';
+  }
+
+  irAEmpleados() {
+    window.location.href = 'http://localhost:8100/listausuarios';
+  }
+
+  irACalendario() {
+    window.location.href = 'http://localhost:8100/horario';
+  }
+
+  irANomina() {
+    window.location.href = 'http://localhost:8100/nomina';
+  }
+
+  irAPerfilAdmin() {
+    window.location.href = 'http://localhost:8100/perfiladmin';
+  }
+}
