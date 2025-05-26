fb1cfc8f5c7ac53dc9d70944179aa77fb6a44bdd
Simón Paipa <simonpaipab@gmail.com>
2025-05-25 21:08:38 -0500

    mejorar: actualizar la página de perfil de administrador con nuevos enlaces de navegación y ajustes de diseño

--- front/src/app/pages/perfiladmin/perfiladmin.page.ts
+++ front/src/app/pages/perfiladmin/perfiladmin.page.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { 
+  peopleOutline, 
+  calendarOutline, 
+  cashOutline,
+  barChartOutline 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-perfiladmin',
@@ -11,9 +18,42 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./perfiladmin.page.scss']
 })
 export class PerfiladminPage {
+  constructor() {
+    addIcons({
+      peopleOutline,
+      calendarOutline,
+      cashOutline,
+      barChartOutline
+    });
+  }
+
   abrirPerfil() {
     console.log('Abriendo perfil...');
     // Aquí podrías abrir un modal, popover, o navegar a otra página.
+  }
+
+  irAPrincipal() {
+    window.location.href = 'http://localhost:8100/principal';
+  }
+
+  irAEmpleados() {
+    window.location.href = 'http://localhost:8100/listausuarios';
+  }
+
+  irACalendario() {
+    window.location.href = 'http://localhost:8100/horario';
+  }
+
+  irANomina() {
+    window.location.href = 'http://localhost:8100/nomina';
+  }
+
+  irAPerfilAdmin() {
+    window.location.href = 'http://localhost:8100/perfiladmin';
+  }
+
+  irADatos() {
+    window.location.href = 'http://localhost:8100/datos';
   }
 }
 
